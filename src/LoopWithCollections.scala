3a530d188709e4569639856a4b4a2ec05b6773fa
Nilesh Devdas <nilesh.devdas@vinsys.com>
2019-05-04 21:24:26 +0530

    Initial Commit1

--- src/LoopWithCollections.scala
+++ src/LoopWithCollections.scala
@@ -1,1 +1,26 @@
 
+// all collection are immutable
+
+val mylist = List(1,2,3,4,5);
+val newlist = mylist.take(4);
+for(element <- mylist){
+  println(element);
+}
+for(newelements <- newlist){
+  println(newelements);
+}
+mylist.foreach((el)=>{
+    println(el);
+});
+
+println(mylist(1));
+println(mylist(4));
+println(mylist.max);
+println(mylist.sum);
+println(mylist.min);
+println(mylist.head);
+println(mylist.tail);
+println(mylist.length);
+println(mylist.take(3));
+println(mylist.takeRight(3));
+println(mylist.reverse);
