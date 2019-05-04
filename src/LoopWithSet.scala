cdbc32c758518933f180c517fbaead333898bd9c
Nilesh Devdas <nilesh.devdas@vinsys.com>
2019-05-04 21:41:49 +0530

    Initial Commit4

--- src/LoopWithSet.scala
+++ src/LoopWithSet.scala
@@ -1,1 +1,23 @@
 
+
+val myset = Set(2, 2, 3,4,4,6 ,7);
+
+println(myset.mkString(","));
+// note the myset(index) is actuall not a index but a value comparison
+/// when i say myset(10) it check if value 10 exists inside the set and returns
+//  a boolean value
+println(myset(10));
+
+// functional methods on set
+println(myset.head);
+println(myset.tail);
+println(myset.size);
+println(myset.take(2));
+
+for(el <- myset){
+  println(el);
+}
+/// this is foreach as function
+myset.foreach((el)=>{
+  println(el);
+});
